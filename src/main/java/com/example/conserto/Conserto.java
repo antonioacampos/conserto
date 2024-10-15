bab65d1d35d0855350344114618ab8728ddb1c6d
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2024-10-15 10:49:37 -0300

    fix readme and cleanup

--- src/main/java/com/example/conserto/Conserto.java
+++ src/main/java/com/example/conserto/Conserto.java
@@ -4,10 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 @Entity
